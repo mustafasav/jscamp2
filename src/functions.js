d90386fade6c8f717cdaabe3d86dc3a4129502c1
mustafa sav <mustafasav@gmail.com>
2021-11-08 18:37:07 +0300

    ders2

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,26 @@
-console.log("dENMEME")
+
+function addToCart(quantity,productName="Elma") {
+//fonksiyona değer gönderilmezse  "Elma" default değerini alır.
+    
+    console.log("Sepete Eklendi: Ürün: "+productName + " ve Adet: "+quantity)
+    
+}
+
+
+//addToCart()
+addToCart(10)
+//addToCart(15)
+
+let sayHello = ()=>{
+    console.log("Hello World")
+}
+
+sayHello()
+
+let sayHello2 = function (isim) {
+    console.log("Hello World 2: " + isim)
+}
+
+sayHello2("Mustafa")
+
+
